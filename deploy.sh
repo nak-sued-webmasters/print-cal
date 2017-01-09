9f21c161ac9bb66bdf04bbd430363103ff1166ba
sybit\ssr <stephan.strittmatter@sybit.de>
2017-01-09 16:18:15 +0100

    deployment

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+gitLastCommit=$(git show --summary --grep="Merge pull request")
+if [[ -z "$gitLastCommit" ]]
+then
+	lastCommit=$(git log --format="%H" -n 1)
+else
+	echo "We got a Merge Request!"
+	#take the last commit and take break every word into an array
+	arr=($gitLastCommit)
+	#the 5th element in the array is the commit ID we need. If git log changes, this breaks. :(
+	lastCommit=${arr[4]}
+fi
+echo $lastCommit
+
+filesChanged=$(git diff-tree --no-commit-id --name-only -r $lastCommit)
+if [ ${#filesChanged[@]} -eq 0 ]; then
+    echo "No files to update"
+else
+    for f in $filesChanged
+	do
+		#do not upload these files that aren't necessary to the site
+		if [ "$f" != ".travis.yml" ] && [ "$f" != "deploy.sh" ] && [ "$f" != "test.js" ] && [ "$f" != "package.json" ]
+		then
+	 		echo "Uploading $f"
+	 		curl --ftp-create-dirs -T $f -u $FTP_USER:$FTP_PASS ftp://fotocontest.nak-sued.de/web/$f
+		fi
+	done
+fi
