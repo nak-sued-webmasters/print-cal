780e532ae235de8f013866b61a707ff8ad5f2de3
stritti <Stephan@st-strittmatter.name>
2017-01-14 23:07:07 +0100

    export to doc

--- src/js/print-cal.js
+++ src/js/print-cal.js
@@ -150,3 +150,55 @@ function renderEvents(cal){
   spinner.stop();
   console.log("done loading Calendar ...");
 }
+
+
+/** HTML to Microsoft Word Export Demo 
+ * This code demonstrates how to export an html element to Microsoft Word
+ * with CSS styles to set page orientation and paper size.
+ * Tested with Word 2010, 2013 and FireFox, Chrome, Opera, IE10-11
+ * Fails in legacy browsers (IE<10) that lack window.Blob object
+ * see: http://stackoverflow.com/questions/36330859/export-html-table-as-word-file-and-change-file-orientation
+ */
+function export2Word(element) {
+ 
+   if (!window.Blob) {
+      alert('Your legacy browser does not support this action.');
+      return;
+   }
+
+   var html, link, blob, url, css;
+   
+   // EU A4 use: size: 841.95pt 595.35pt;
+   // US Letter use: size:11.0in 8.5in;
+   
+   css = (
+     '<style>' +
+     '@page WordSection1{size: 841.95pt 595.35pt;mso-page-orientation: landscape;}' +
+     'div.WordSection1 {page: WordSection1;}' +
+     '#table{border-collapse:collapse;}td{border:1px gray solid;width:5em;padding:2px;}'+
+     '</style>'
+   );
+   
+   html = element.innerHTML;
+   $(html).contents().each(function() {
+    if(this.nodeType === Node.COMMENT_NODE) {
+        $(this).remove();
+    }
+   });
+   blob = new Blob(['\ufeff', css + html], {
+     type: 'application/msword'
+   });
+   url = URL.createObjectURL(blob);
+   link = document.createElement('A');
+   link.href = url;
+   // Set default file name. 
+   // Word will append file extension - do not add an extension here.
+   link.download = 'Termine';   
+   document.body.appendChild(link);
+   if (navigator.msSaveOrOpenBlob ) navigator.msSaveOrOpenBlob( blob, 'Termine.docx'); // IE10-11
+   		else link.click();  // other browsers
+   document.body.removeChild(link);
+ };
+
+
+
